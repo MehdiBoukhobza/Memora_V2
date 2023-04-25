4c617d76249075332c2a99696b4dda41589e2496
El Mehdi Boukhobza <elmehdiboukhobza2002@gmail.com>
2023-04-25 15:15:00 +0100

    New functionalities added

---
+++ src/main/java/com/example/memora_v2/Settings.java
@@ -0,0 +1,15 @@
+package com.example.memora_v2;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Settings extends Navbar {
+
+    static float quickPractice = 10;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        super.initialize(url, resourceBundle);
+
+    }
+}
