62d7939ac6bef294d0b817856c788c3abb50639e
El Mehdi Boukhobza <elmehdiboukhobza2002@gmail.com>
2023-04-26 23:43:18 +0100

    New functionalities added

---
+++ src/main/java/com/example/memora_v2/Account.java
@@ -0,0 +1,51 @@
+package com.example.memora_v2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+public class Account extends Navbar {
+
+    @FXML
+    private Label nameLabel;
+    @FXML
+    private Label labelBio;
+    @FXML
+    private Label numberChosenQuick;
+    @FXML
+    private Label numberChosenMedium;
+
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        super.initialize(url, resourceBundle);
+        displayAll();
+    }
+
+    public void displayAll(){
+        DataBase connectNow = new DataBase();
+        Connection connectDB = connectNow.getConnection();
+
+        String connectQuery = " SELECT FullName, Bio FROM user";
+
+        try {
+            Statement statement = connectDB.createStatement();
+            ResultSet queryOutput = statement.executeQuery(connectQuery);
+
+            while (queryOutput.next()) {
+                nameLabel.setText(queryOutput.getString("FullName"));
+                labelBio.setText(queryOutput.getString("Bio"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        numberChosenQuick.setText(String.valueOf(Settings.quickPractice));
+        numberChosenMedium.setText(String.valueOf(Settings.mediumPractice));
+
+    }
+}
